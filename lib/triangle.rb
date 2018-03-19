d6eefa8d4bc6249c99dcac11fd59271e06e8029a
Eric Kim <hyeokjungkim@gmail.com>
2018-03-18 23:03:48 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,7 @@ class Triangle
   end
 
   def kind
+    binding.pry
     if @side1 > 0 && @side2 > 0 && @side3 > 0
       if @side1 + @side2 > @side3 && @side2 + @side3 > @side1 && @side3 + @side1 > @side2
         if @side1 == @side2 && @side2 == @side3
